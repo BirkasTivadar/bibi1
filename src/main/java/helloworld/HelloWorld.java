51c8fca96c45b66423be3f2aeb145fc245d142bd
BirkasTivadar <birkastivadar@gmail.com>
2021-03-12 16:45:22 +0100

    Hello

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println("Hello github");
+    }
+}
